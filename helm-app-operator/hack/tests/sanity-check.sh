2a7cbfc0c3f4f1eeb2d28552665670822c1249d5
Joe Lanford <joe.lanford@gmail.com>
2018-10-19 22:27:12 -0400

    cmd/*,pkg/*,version,hack/tests: license headers and error lowercasing

--- helm-app-operator/hack/tests/sanity-check.sh
+++ helm-app-operator/hack/tests/sanity-check.sh
@@ -3,11 +3,8 @@ set -ex
 
 go vet ./...
 
-# TODO(joelanford): add license headers to files
-#./hack/check_license.sh
-
-# TODO(joelanford): fix case of error messages
-#./hack/check_error_case.sh
+./hack/check_license.sh
+./hack/check_error_case.sh
 
 # Make sure repo is in clean state
 git diff --exit-code
