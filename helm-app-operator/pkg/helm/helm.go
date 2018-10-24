05332031afdbbe91814473faf42db1dad2e2065b
Joe Lanford <joe.lanford@gmail.com>
2018-10-23 21:13:02 -0400

    pkg/helm: persist release owner in release config value

--- helm-app-operator/pkg/helm/helm.go
+++ helm-app-operator/pkg/helm/helm.go
@@ -73,6 +73,8 @@ const (
 
 	annotationReleaseName          = "helm.operator-sdk/release-name"
 	annotationUseNameAsReleaseName = "helm.operator-sdk/use-name-as-release-name"
+
+	valueResourceUID = "helm.operator-sdk/resource-uid"
 )
 
 // Installer can install and uninstall Helm releases given a custom resource
@@ -222,7 +224,22 @@ func (c installer) ReconcileRelease(r *unstructured.Unstructured) (*unstructured
 	if err != nil {
 		return r, needsUpdate, fmt.Errorf("failed to parse values: %s", err)
 	}
-	config := &cpb.Config{Raw: string(cr)}
+	uid := cpb.Value{
+		Value: string(r.GetUID()),
+	}
+	config := &cpb.Config{
+		Raw: string(cr),
+
+		// Store the resource UID as a config value in the release.
+		// This is helpful to determine release ownership, even in the
+		// case where a status update for the release fails. By setting
+		// it as a config value, it also gets saved in the storage
+		// backend used to record releases as they are installed and
+		// updated.
+		Values: map[string]*cpb.Value{
+			valueResourceUID: &uid,
+		},
+	}
 	logrus.Debugf("Using values: %s", config.GetRaw())
 
 	err = processRequirements(chart, config)
@@ -268,9 +285,8 @@ func (c installer) ReconcileRelease(r *unstructured.Unstructured) (*unstructured
 		needsUpdate = true
 		logrus.Infof("Installed release for %s release=%s", ResourceString(r), updatedRelease.GetName())
 
-	} else if status.Release == nil {
-		// If the object has no release status, it does not own the release,
-		// so return an error.
+	} else if !isReleaseOwner(r, latestRelease) {
+		// If this object is not the release owner, return an error.
 		return r, needsUpdate, fmt.Errorf("install error: release \"%s\" already exists", releaseName)
 	} else {
 		candidateRelease, err := c.getCandidateRelease(tiller, releaseName, chart, config)
@@ -308,13 +324,6 @@ func (c installer) ReconcileRelease(r *unstructured.Unstructured) (*unstructured
 // UninstallRelease accepts a custom resource, uninstalls the existing Helm release
 // using Tiller, and returns the custom resource with updated `status`.
 func (c installer) UninstallRelease(r *unstructured.Unstructured) (*unstructured.Unstructured, error) {
-	// If the object has no release status, it does not own the release,
-	// so there's nothing to do.
-	status := v1alpha1.StatusFor(r)
-	if status.Release == nil {
-		return r, nil
-	}
-
 	releaseName := getReleaseName(r)
 
 	// Get history of this release
@@ -326,6 +335,11 @@ func (c installer) UninstallRelease(r *unstructured.Unstructured) (*unstructured
 	// If there is no history, the release has already been uninstalled,
 	// so there's nothing to do.
 	if len(h) == 0 {
+		return r, nil
+	}
+
+	// If this object is not the release owner, there's nothing to do.
+	if !isReleaseOwner(r, h[0]) {
 		return r, nil
 	}
 
@@ -494,6 +508,10 @@ func getReleaseName(r *unstructured.Unstructured) string {
 	// An empty release name will be populated automatically by tiller
 	// during installation
 	return ""
+}
+
+func isReleaseOwner(r *unstructured.Unstructured, rel *release.Release) bool {
+	return string(r.GetUID()) == rel.GetConfig().GetValues()[valueResourceUID].GetValue()
 }
 
 func notFoundErr(err error) bool {
